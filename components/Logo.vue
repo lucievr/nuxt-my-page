621f23bbb455f3c32809a6fda474ff2108947b57
Lucie <46979603+lucievr@users.noreply.github.com>
2019-04-02 12:59:35 +0200

    Complete page

---
+++ components/Logo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="lucie" src="~/static/lucie.png" alt="the logo">
+</template>
+
+<style>
+.lucie {
+  border-radius: 10px;
+}
+
+</style>
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+      {
+        id: 'about',
+        title: 'About me',
+        content: 'Hello and welcome to my page! My name is Lucie and I am a Fullstack Web Developer. '
+      },
+      {
+        id: 'work',
+        title: 'Work & Projects',
+        content: 'Here is a selection of my projects to showcase my wide range of skills and developer experience:'
+      },
+      {
+        id: 'contact',
+        title: 'Get in touch',
+        content: 'Contact me here'
+      }
+    ]
+  })
